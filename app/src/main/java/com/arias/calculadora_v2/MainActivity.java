afc64076fc34337148ab2746802b2e8f36687088
manu0120 <manu0120sp@gmail.com>
2021-11-12 20:33:49 +0100

    Duodecimo comentario

--- app/src/main/java/com/arias/calculadora_v2/MainActivity.java
+++ app/src/main/java/com/arias/calculadora_v2/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //super commit
-    //2, 3, 4, 6, 7, 8, 9.10
+    //2, 3, 4, 6, 7, 8, 9.10, 11
 
 
     @Override
